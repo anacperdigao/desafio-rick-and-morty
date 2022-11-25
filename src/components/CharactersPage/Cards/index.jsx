3fb6e8c3f2afc752163f0de696ef1ff95a7c9ea4
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-25 13:49:58 -0300

    feat: creating component Cards

---
+++ src/components/CharactersPage/Cards/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { lightGray, neonGreen, red } from '../../UI/colorVar'
+import * as S from './styles'
+
+
+const Cards = ({image, name, status, species, gender, origin}) => {
+
+    return (
+        <S.CardCharacter>
+            <S.ImageCard src={image} alt={name} />
+            <S.DivInfo>
+                <S.TitleName>{name}</S.TitleName>
+
+                {status === "Alive" && <S.TagStatus style={{backgroundColor: `${neonGreen}`}}>{status}</S.TagStatus>}
+                {status === "Dead" && <S.TagStatus style={{backgroundColor: `${red}`}}>{status}</S.TagStatus>}
+                {status === "unknown" && <S.TagStatus style={{backgroundColor: `${lightGray}`}}>{status}</S.TagStatus>}
+                
+                <S.DivLabelParagraph>
+                    <S.Label>Species:</S.Label>
+                    <S.Props>{species}</S.Props>
+                </S.DivLabelParagraph>
+
+                <S.DivLabelParagraph>
+                    <S.Label>Gender:</S.Label>
+                    <S.Props>{gender}</S.Props>
+                </S.DivLabelParagraph>
+
+                <S.DivLabelParagraph>
+                    <S.Label>Origin:</S.Label>
+                    <S.Props>{origin}</S.Props>
+                </S.DivLabelParagraph>
+
+            </S.DivInfo>
+        </S.CardCharacter>
+    )
+}
+
+export default Cards
