f0596545148282bb92484651bb2cda809d6b5510
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-25 15:08:25 -0300

    style: style component Character

---
+++ src/components/Character/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+
+export const ShowCharacter = styled.div`
+    position: fixed;
+    z-index: 1;
+    padding-top: 200px;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0, 0, 0, 0.4);
+    border: none;
+    line-height: 26px;
+` 
+
+export const Dialog = styled.dialog`
+    border: none;
+    border-radius: 10px;
+    max-width: 320px;
+    padding: 20px;
+    margin: auto;
+`
+
