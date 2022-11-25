940231dc8af9b31b007d3c75004f3beee0ab80e4
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-25 13:00:37 -0300

    feat: conditional rendering component InitialPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GlobalStyle } from "./components/GlobalStyle";
+import InitialPage from "./components/InitialPage";
+import { MainContext } from "./context/main";
 
 
 function App() {
+
+  const [ pages, pageState, setPageState ] = useContext(MainContext)
+
   return (
     <div className="App">
       <GlobalStyle />
-      
+
+      { pageState === 'Initial' && <InitialPage /> }
+
     </div>
   );
 }
