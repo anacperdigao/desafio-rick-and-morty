c53b5fa91a6dbc962a7b4b895bc1dbcd714a2c09
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-25 12:23:10 -0300

    feat: implementing GlobalStyle

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { GlobalStyle } from "./components/GlobalStyle";
+
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,25 @@
+import {createGlobalStyle} from 'styled-components';
+import { bgDarkGreen } from './UI/colorVar';
+
+
+
+export const GlobalStyle = createGlobalStyle`
+
+* {  
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+
+body {
+    background-color: ${bgDarkGreen};
+    background-repeat: no-repeat;
+    background-size: cover;
+    box-sizing: border-box;
+    height: 100vh;
+    width: 100%;
+}
+`;
+
