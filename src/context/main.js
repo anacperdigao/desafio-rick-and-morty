b19065d76a85670f2d7f0b64785b01b250c0ece5
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-25 13:26:59 -0300

    feat: adding states and getting API data

--- src/context/main.js
+++ src/context/main.js
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from "react"
-
+import React, { createContext, useEffect, useState } from "react"
+import api from "../api/api";
 
 
 //-------------- Criando o contexto
@@ -11,15 +11,40 @@ MainContext.displayName = 'MainContext'
 //-------------- Criando o Provider
 export const MainProvider = ({children}) => {
 
-    const pages = ['Initial', 'Characters', 'Episodes']
+    const pages = ['Initial', 'Characters']
     const [pageState, setPageState] = useState(pages[0])
-    
+    const [apiData, setApiData] = useState({})
+    const [name, setName] = useState("")
+    const [status, setStatus] = useState("")
+    const [gender, setGender] = useState("")
+    const [pageApi, setPageApi] = useState(1)
+
+
+    useEffect( () => {
+        api
+        .get(`/character/?page=${pageApi}&name=${name}&status=${status}&gender=${gender}`)
+        .then((response) => {
+            setApiData(response.data); 
+        })
+        .catch((erro) => {alert(`Ops! Ocorreu um erro: ${erro}`)})
+    }, [pageApi, name, status, gender]);
+
 
     return(
         <MainContext.Provider value={[
             pages,
             pageState,
             setPageState,
+            apiData, 
+            setApiData,
+            name, 
+            setName,
+            status, 
+            setStatus,
+            gender, 
+            setGender,
+            pageApi, 
+            setPageApi,
         ]}>
             {children}
         </MainContext.Provider>
