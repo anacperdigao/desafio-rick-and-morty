fa2f09820bc8a4c18ac4dd96251ab6563ff02146
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-26 12:19:01 -0300

    change: changing layout InitialPage

--- src/components/InitialPage/index.jsx
+++ src/components/InitialPage/index.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import * as S from "./styles"
-import rick from '../../assets/images/rickandmorty.png'
 import { MainContext } from '../../context/main'
+import Background from '../../assets/images/wallpaper1.jpg'
 
 const InitialPage = () => {
 
@@ -11,12 +11,13 @@ const InitialPage = () => {
 
     <S.DivPage>
 
-        <S.DivTitles>
-            <S.Title1>Greetings!</S.Title1>
-            <S.Title2>Click on the image to discover a new world...</S.Title2>
-        </S.DivTitles>
+        <S.ImgBackground src={Background} alt="Background" />
 
-        <S.ImageLogo src={rick} alt="logo" onClick={() => setPageState(pages[1])}/>
+        <S.DivInfo>
+          <S.Title>Rick and Morty</S.Title>
+          <S.SubTitle>Search for your favorite characters</S.SubTitle>
+          <S.Button onClick={() => setPageState(pages[1])}>KNOW MORE</S.Button>
+        </S.DivInfo>
 
     </S.DivPage>
   )
