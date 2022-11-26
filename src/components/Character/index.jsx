aeb8f7d493b456bb95e1c84ad8aca2d86c3a1580
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-25 21:39:44 -0300

    feat: creating component character

--- src/components/Character/index.jsx
+++ src/components/Character/index.jsx
@@ -1,33 +1,59 @@
-import React, { useContext } from 'react'
-import { MainContext } from '../../context/main'
+import React from 'react'
+import { lightGray, neonGreen, red } from '../UI/colorVar'
 import * as S from './styles'
 
 
-const Character = () => {
-
-    const [ pages,
-            pageState,
-            setPageState,
-            apiData, 
-            setApiData,
-            name, 
-            setName,
-            status, 
-            setStatus,
-            gender, 
-            setGender,
-            pageApi, 
-            setPageApi,
-            showCharacter, 
-            setShowCharacter,
-        ] = useContext(MainContext)
+const Character = ({image, name, status, species, gender, origin, 
+                    showCharacter, setShowCharacter, location, subspecies}) => {
+        
 
     return (
 
         <S.ShowCharacter>
             <S.Dialog open={showCharacter}>
 
-            <button onClick={() => setShowCharacter(false)}>Back</button>
+                <S.CardCharacter>
+                    <S.ImageCard src={image} alt={name} />
+
+                    <S.DivInfo>
+                        
+                        <S.TitleName>{name}</S.TitleName>
+
+                        {status === "Alive" && <S.TagStatus style={{backgroundColor: `${neonGreen}`}}>{status}</S.TagStatus>}
+                        {status === "Dead" && <S.TagStatus style={{backgroundColor: `${red}`}}>{status}</S.TagStatus>}
+                        {status === "unknown" && <S.TagStatus style={{backgroundColor: `${lightGray}`}}>{status}</S.TagStatus>}
+                        
+                        <S.DivLabelParagraph>
+                            <S.Label>Species:</S.Label>
+                            <S.Props>{species}</S.Props>
+                        </S.DivLabelParagraph>
+
+                        <S.DivLabelParagraph>
+                            <S.Label>Subspecies:</S.Label>
+                            <S.Props>{subspecies === "" ? "None" : subspecies}</S.Props>
+                        </S.DivLabelParagraph>
+
+                        <S.DivLabelParagraph>
+                            <S.Label>Gender:</S.Label>
+                            <S.Props>{gender}</S.Props>
+                        </S.DivLabelParagraph>
+
+                        <S.DivLabelParagraph>
+                            <S.Label>Origin:</S.Label>
+                            <S.Props>{origin}</S.Props>
+                        </S.DivLabelParagraph>
+
+                        <S.DivLabelParagraphColumn>
+                            <S.Label>Last known location:</S.Label>
+                            <S.Props>{location}</S.Props>
+                        </S.DivLabelParagraphColumn>
+
+                    </S.DivInfo>
+
+                </S.CardCharacter>
+
+                <S.Button onClick={() => setShowCharacter(false)}>Back</S.Button>
+
             </S.Dialog>
         </S.ShowCharacter>
 
