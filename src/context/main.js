dff1dbd4caa25c3f9291f6371dee487c06a0eca9
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-27 14:49:02 -0300

    debug: debugging context

--- src/context/main.js
+++ src/context/main.js
@@ -3,60 +3,60 @@ import api from "../api/api";
 
 
 //-------------- Criando o contexto
-export const MainContext = createContext();
-MainContext.displayName = 'MainContext';
+export const MainContext = createContext()
+MainContext.displayName = 'MainContext'
 
 
 
 //-------------- Criando o Provider
 export const MainProvider = ({children}) => {
 
-  const pages = ['Initial', 'Characters'];
-  const [pageState, setPageState] = useState(pages[0]);
-  const [apiData, setApiData] = useState({});
-  const [name, setName] = useState("");
-  const [status, setStatus] = useState("");
-  const [gender, setGender] = useState("");
-  const [pageApi, setPageApi] = useState(1);
+  const pages = ['Initial', 'Characters']
+  const [pageState, setPageState] = useState(pages[0])
+  const [apiData, setApiData] = useState({})
+  const [name, setName] = useState("")
+  const [status, setStatus] = useState("")
+  const [gender, setGender] = useState("")
+  const [pageApi, setPageApi] = useState(1)
 
 
   useEffect( () => {
     api
     .get(`/character/?page=${pageApi}&name=${name}&status=${status}&gender=${gender}`)
     .then((response) => {
-        setApiData(response.data);
+        setApiData(response.data)
     })
     .catch((erro) => {
-      alert(`Ops! The name "${name}" doesn't exist!`);
-      setName("");
+      alert(`Ops! The name "${name}" doesn't exist!`)
+      setName("")
     })
-  }, [pageApi, name, status, gender]);
+  }, [pageApi, name, status, gender])
 
 
   const handlePrevious = () => {
     if(pageApi <= 1) return
     setPageApi(parseInt(pageApi - 1))
-  };
+  }
 
   const handleNext = () => {
     if(apiData.info.next === null) return
     setPageApi(parseInt(pageApi + 1))
-  };
+  }
 
   const handleFirst = () => {
     setPageApi(1)
-  };
+  }
 
   const handleLast = () => {
     setPageApi(apiData.info.pages)
-  };
+  }
 
   const handleClear = () => {
     setName("")
     setGender("")
     setStatus("")
     setPageApi(1)
-  };
+  }
 
 
   return(
